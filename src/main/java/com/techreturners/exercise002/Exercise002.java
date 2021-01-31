ce3aa8a2c4bd0386b7a690b885f9997da267f4e6
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-31 13:52:04 +0000

    implementing canWatchFilm to pass test Exercise 2

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -10,8 +10,11 @@ public class Exercise002 {
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        
+        if (person.getAge() >= ageLimit){
+            return true;
+        }
         return false;
     }
     
